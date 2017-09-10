7635b6b55d7fb77a8383f5228f5115fd2a60d73d
Rahul <shukla2009@gmail.com>
2017-09-10 21:14:42 +0530

    adding readme

--- run.sh
+++ run.sh
@@ -4,3 +4,10 @@ docker run --name myproj \
  -e MYSQL_HOST=172.17.0.4 \
  -e MYSQL_PORT=3306 \
  -d shukla2009/php
+
+
+
+ docker run --name benchmark \
+  -e MYSQL_HOST=172.17.0.4 \
+  -e MYSQL_PORT=3306 \
+  -d shukla2009/php-benchmark
